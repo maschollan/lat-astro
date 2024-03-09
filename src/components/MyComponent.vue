30a7f52e66fd913cd28d36d33cf80a898b8a907f
Mas Chollan <kholan.childs404@gmail.com>
2024-03-09 13:15:31 +0700

    Menambah Vue dan beberapa halaman

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Test Halaman Vue</h1>
+        <p>Hasil input : {{ teks }}</p>
+
+        <input type="text" v-model="teks"/>
+        <ol>
+            <li v-for="h in hobi" :key="h">{{ h }}</li>
+        </ol>
+    </div>
+</template>
+
+<script>
+import "../styles/global.css";
+
+export default {
+    name: "MyComponent",
+    data() {
+        return {
+            teks: "",
+            hobi: ["makan", "tidur", "nyanyi"],
+        };
+    },
+};
+</script>
